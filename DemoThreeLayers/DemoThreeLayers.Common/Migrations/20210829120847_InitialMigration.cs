4feefc42d24c227561ca5d4166d3c80958d834d0
HankLin311 <Hank.lin.311@gmail.com>
2021-08-29 21:44:08 +0800

    Revert "Revert "Add Template""
    This reverts commit 5af8d64f67023b73c9c062462e2ac57822e3a1b5.

---
+++ DemoThreeLayers/DemoThreeLayers.Common/Migrations/20210829120847_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoThreeLayers.Common.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    IdentityNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsResign = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => new { x.EmployeeId, x.IdentityNumber });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ DemoThreeLayers/DemoThreeLayers.Repository/EmployeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using DemoThreeLayers.Common.Entities;
+using DemoThreeLayers.Repository.Interfaces;
+using RehabusSystem.Common.DbContexts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoThreeLayers.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _context.Employee;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _context.Employee.FirstOrDefault(n => n.EmployeeId == id);
+        }
+    }
+}
